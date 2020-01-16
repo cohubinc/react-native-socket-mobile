aacb3f4bec732fd232a452d62f7c81d1d2134478
Jeff Lewis <mail@jeffreylewis.me>
2020-01-16 13:53:57 -0600

    Disable sdk imports

--- android/src/main/java/com/react_native_socket_mobile/Listener.java
+++ android/src/main/java/com/react_native_socket_mobile/Listener.java
@@ -1,15 +1,15 @@
-package com.react_native_socket_mobile;
+// package com.react_native_socket_mobile;
 
-import com.socketmobile:capture;
+// import com.socketmobile.capture;
 
-public class Listener extends CaptureClient.Listener {
-  public void onData(DataEvent event) {
-  }
+// public class Listener extends CaptureClient.Listener {
+//   public void onData(DataEvent event) {
+//   }
   
-  public void onDeviceStateEvent(DataStateEvent event) {
-  }
+//   public void onDeviceStateEvent(DataStateEvent event) {
+//   }
 
-  public void onError(CaptureError error) {
+//   public void onError(CaptureError error) {
 
-  }
-}
+//   }
+// }
